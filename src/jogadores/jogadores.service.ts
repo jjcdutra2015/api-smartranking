64697e576458cce1282a2a13d48baaa871c41101
Julio Dutra <jjcdutra@gmail.com>
2020-10-09 10:46:55 -0300

    Implementando busca e deleção por email

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './intefaces/jogador.interface';
 import { v1 as uuid }  from 'uuid';
@@ -11,7 +11,35 @@ export class JogadoresService {
     private readonly logger = new Logger(JogadoresService.name)
 
     async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
-        this.criar(criarJogadorDto)
+        const { email } = criarJogadorDto
+
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email)
+
+        if (jogadorEncontrado) {
+            await this.atualizar(jogadorEncontrado, criarJogadorDto)
+        } else {
+            await this.criar(criarJogadorDto)
+        }
+    }
+
+    async consultarTodosJogadores(): Promise<Jogador[]> {
+        return this.jogadores
+    }
+
+    async consultarJogadorPeloEmail(email:string): Promise<Jogador> {
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email)
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`Jogador com o e-mail ${email} não encontrato`)
+        }
+        return jogadorEncontrado
+    }
+
+    async deletarJogador(email: string): Promise<void> {
+        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`Jogador com o e-mail ${email} não encontrato`)
+        }
+        this.jogadores = this.jogadores.filter(jogador => jogador.email !== jogadorEncontrado.email)
     }
 
     private criar(criarJogadorDto: CriarJogadorDto): void {
@@ -29,5 +57,11 @@ export class JogadoresService {
         this.logger.log(`criarJogadorDto: ${JSON.stringify(jogador)}`)
 
         this.jogadores.push(jogador)
+    }
+
+    private atualizar(jogadorEncontrado: Jogador, criarJogadorDto: CriarJogadorDto): void {
+        const { nome } = criarJogadorDto
+
+        jogadorEncontrado.nome = nome
     }
 }
