63f61cec3506d7cd0635de16626a8ed753e32be2
Julio Dutra <jjcdutra@gmail.com>
2020-10-13 15:55:27 -0300

    Implementação inicial entidade Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.qbstg.mongodb.net/smartranking?retryWrites=true&w=majority',
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
-    JogadoresModule],
+    JogadoresModule,
+    CategoriasModule],
   controllers: [],
   providers: [],
 })
